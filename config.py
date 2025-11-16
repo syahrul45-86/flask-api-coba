f350735639218da7a9bf4e4605d91669c64d9efd
syahrul45-86 <74806658+syahrul45-86@users.noreply.github.com>
2025-11-16 23:24:05 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+load_dotenv()
 
 db = SQLAlchemy()
-
-load_dotenv()  # baca file .env
 
 def create_app():
     app = Flask(__name__)
@@ -16,16 +16,9 @@ def create_app():
     DB_PORT = os.getenv("DB_PORT")
     DB_NAME = os.getenv("DB_NAME")
 
-    # Debug print (nonaktifkan di vercel)
-    print("ENV:", DB_HOST, DB_PORT, DB_NAME)
-
-    DATABASE_URI = (
-        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
-        f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
     return app
