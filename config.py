17db47d1428a301b0838a506cb291e4f005703d9
syahrul45-86 <74806658+syahrul45-86@users.noreply.github.com>
2025-11-16 23:28:32 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-from dotenv import load_dotenv
 import os
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()   # penting untuk local, tapi Vercel juga support env internal
 
 db = SQLAlchemy()
 
@@ -16,9 +16,14 @@ def create_app():
     DB_PORT = os.getenv("DB_PORT")
     DB_NAME = os.getenv("DB_NAME")
 
+    if not all([DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME]):
+        raise Exception("❌ ENVIRONMENT VARIABLES NOT LOADED")
+
     app.config["SQLALCHEMY_DATABASE_URI"] = (
         f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
+
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
     return app
